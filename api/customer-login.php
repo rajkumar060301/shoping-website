d9bd993f870f4aabcf627552305e757c58753325
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-12 16:14:33 +0530

    customer login changes

--- api/customer-login.php
+++ api/customer-login.php
@@ -1,22 +1,16 @@
 <?php
 include "../config/config.php";
-
 session_start();
 $user = $_POST['username'];
 $password = $_POST['password'];
-
-
 $read_query = "SELECT * from `customer` where `customer_email`='".$user."' and `customer_password`='".$password."'";
    $result = $myConnection->query($read_query);
    if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-        $_SESSION['teamID']=$row['id'];
-
+        $_SESSION['teamID']=$row['customer_id'];
                header("location:../home.php");
-
             }
             else{
-
                 echo "<script>alert('You entered Wrong email id or Password')</script>";
                 echo "<script>location.href='../customer-login.html'</script>";
 
