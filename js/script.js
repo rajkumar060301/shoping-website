dea35bf6a35a4392cfb7a16dd6e43daaaca26ff8
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-13 16:16:32 +0530

    write code of ajax

--- js/script.js
+++ js/script.js
@@ -1,22 +1,53 @@
 $(document).ready(function() {
+    function loadData(){
+	                    
+        $.ajax({
+            url : "api/cart.php",
+            type : "POST",
+            success: 
+                function(data){
+                    $("#content-id").html(data);
+                
+            }
+           });
+    };
+
+    $("#home").on('click',function(){
+        $.ajax({
+            url : "home.php",
+            type : "POST",
+            success : function(data){   
+                $("#body-part").html(data);
 
 
-    $("#product-id-1").click(function(e){
-        e.preventDefault();
+
+            }
+
+        });
+    })
+
+    $("#product-id-1").click(function(){
+        // e.preventDefault();
+
         var name = $("#product-name-1").text();
         var desc = $("#desc-1").text();
-        // var cost = $("#cost-1").val();
+        var quantity = $("#quantity-item-1").val();
         var cost = $("#cost-1").text();
+        var images = $("#product-img1").attr('src');
+        // alert(images);
         $.ajax({
             url : "api/add-product.php",
             type : "POST",
             data : {
                 name : name,
                 desc : desc,
-                cost : cost
+                quantity : quantity,
+                cost : cost,
+                images : images
             },
-            success : function(data){
-                alert(data);
+            success : function(){
+                // alert(data);
+                $("#myform").trigger('reset');
 
             }
 
@@ -24,21 +55,40 @@ $(document).ready(function() {
         });
     });
 
-    $("#card").click(function(e){
-        e.preventDefault();
+    $("#card").click(function(){
+        // e.preventDefault();
         // loadData();
-        function loadData(){
-	                    
-            $.ajax({
-                url : "api/cart.php",
-                type : "POST",
-                success: 
-                    function(data){
-                        $("#content-id").html(data);
-                    
-                }
-               });
-        }
+
         loadData();
     });
+
+        $('.quantity').on('click', '.plus', function(e) {
+            let $input = $(this).prev('input.qty');
+            let val = parseInt($input.val());
+            $input.val( val+1 ).change();
+        });
+ 
+        $('.quantity').on('click', '.minus', 
+            function(e) {
+            let $input = $(this).next('input.qty');
+            var val = parseInt($input.val());
+            if (val > 0) {
+                $input.val( val-1 ).change();
+            } 
+        });
+
+
+        	            	//Delete data from database
+					$(document).on('click','.delbutton',function(){
+						var studentId = $(this).data('id');
+						$.ajax({
+							url : "api/remove-product.php",
+							type : "post",
+							data : {id : studentId},
+							success : function(){
+								loadData();
+							}
+						})
+
+					})
 });
