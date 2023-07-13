e451259ce5b02f16f8a1184a6b2d89f81730ab14
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-13 16:15:31 +0530

    worked on car add product in card.

--- api/cart.php
+++ api/cart.php
@@ -10,8 +10,8 @@ a:hover{text-decoration:none; color:white}
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- <link rel="styleseet" type="text/css" href="./css/css/bootstrap.min.css">  -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+    <link rel="styleseet" type="text/css" href="./css/css/bootstrap.min.css"> 
 
 
 
@@ -26,49 +26,59 @@ $(document).ready(function () {
 </head>
 <body>
   
-
-<table id="example" class="display" style="width:100%">
+<table id="example" class="display" style="width:100%;text-align:center">
     <thead>
         <tr>
-            <th>Product ID</th>
-            <th>Product Name</th>
-            <th>Product Description</th>
-            <th>Product Cost</th>
-            <th>Delete Product</th>
+            <th style="width: 10%;">Product Image</th>
+            <th style="width: 10%;">Product ID</th>
+            <th  style="width: 10%;">Product Name</th>
+            <th  style="width: 40%;">Product Description</th>
+            <th  style="width: 10%;">Product quantity</th>
+            <th  style="width: 10%;">Product Cost</th>
+            <th  style="width: 10%;">Delete Product</th>
         </tr>
+        
     </thead>
     <tbody>
 
-<?php
-include '../config/config.php';
+        <?php
+        include '../config/config.php';
 
-$query = "select * from cart";
-$result = mysqli_query($myConnection,$query);
-		if(mysqli_num_rows($result)>0){
+        $query = "select * from cart";
+        $sum = 0;
+        $result = mysqli_query($myConnection,$query);
+                if(mysqli_num_rows($result)>0){
 
-			while($row = mysqli_fetch_assoc($result)){
-				?>
-			
-	
-        <tr>
-            <td><?php echo $row["product_id"];?></td>
-            <td><?php echo $row["product_name"];?></td>
-            <td><?php echo $row["product_description"];?></td>
-            <td><?php echo $row["product_cost"];?></td>
+                    while($row = mysqli_fetch_assoc($result)){
 
-            <td><button  data-id="<?php echo $row['product_id']; ?>" class="btn btn-danger delbutton">Delete</button></td>
+                        ?>
+
+                <tr>
+                <td><img src="<?php echo $row["product_img"];?>" style="height:100px;width:100px"></td>
+                    <td><?php echo $row["product_id"];?></td>
+                    <td><?php echo $row["product_name"];?></td>
+                    <td><?php echo $row["product_description"];?></td>
+                    <td><?php echo $row["product_quantity"];?></td>
+                    <td><?php echo $row["product_cost"];?></td>
+                    <?php $sum += $row["product_cost"];?>
+                    <td><button  data-id="<?php echo $row['product_id']; ?>" class="btn btn-danger delbutton">Remove</button></td>
+            </tr>
+ 
             <?php
-            
 
-     echo   "</tr>";
-			}
-    echo "</tbody>";
- echo "</table>";
-		}
-		?>
-        
+                    }
+                    //  echo $sum;
+            echo "</tbody>";
+            echo "<tr><td colspan='5' style='color: red;background-color:yellow;'>Total Amount</td><td class='btn btn-success' style='color: red;'> $sum</td></tr>";
+
+        echo "</table>";
+
+                }
+                ?>
 
   </div>
 </div>
 </body>
+<script type="text/javascript" src="js/script.js"></script>
+
 </html>
