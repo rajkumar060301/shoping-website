a0e8a10eb9ad9968e63cb6de414b73e0104f7bf3
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-14 15:46:44 +0530

    faltu file for testing use this file

--- check.php
+++ check.php
@@ -1,20 +1,65 @@
-<?php
-session_start();
-$id = $_SESSION['teamID'];
-echo $id;
-echo "hello world";
-// include "config/config.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check code</title>
+    <script
+  src="https://code.jquery.com/jquery-3.7.0.js"
+  integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
+  crossorigin="anonymous"></script>
+</head>
+<body>
 
-// $fetch_query = "SELECT * FROM customer where id=".$id;
+<button class="cart">Button 1</button>
 
-// $data_register = mysqli_query($myConnection, $fetch_query);
+    <button class="cart">Button 2</button>
 
-// if(mysqli_num_rows($data_register)>0){
+    <button class="cart">Button 3</button>
 
-//     $row_data = mysqli_fetch_array($data_register); 
-        
-// } else {
-//     echo "Record Not found";
-// }
+    <button class="cart">Button 4</button>
 
-?>
+    <button class="cart">Button 5</button>
+
+</body>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.cart').each(function(index) {
+        var button = $(this);
+        button.attr('id',(index + 1)); // Assign ID with 'product-' prefix and index
+
+        button.on('click', function() {
+            var id = $(this).attr('id');
+            var product_name = "product-name-" + id;
+            var product_url = "product-img-" + id;
+            var product_cost = "product-price-" + id;
+            var product_desc = "product-desc-" + id;
+            var product_quantity = "product-qty-" + id;
+
+            $.ajax({
+            url : "api/add-product.php",
+            type : "POST",
+            data : {
+                name : product_name,
+                desc : product_desc,
+                quantity : product_quantity,
+                cost : product_cost,
+                images : product_url
+            },
+            success : function(){
+                // alert(data);
+                $("#myform").trigger('reset');
+
+            }
+
+
+        });
+        });
+    });
+});
+
+
+</script>
+
+
+</html>
