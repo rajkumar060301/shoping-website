b1bc1d56966da3df941df5d66ce3294b8fdb9790
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-14 15:46:00 +0530

    worked on customer home page in front-end modified some requeriment

--- home.php
+++ home.php
@@ -35,6 +35,7 @@ if(mysqli_num_rows($data_register)>0){
             <li style="margin-left: 60%;"><a href="#">Welcome! <?php echo $row_data['customer_name'] ?></a><i class="bi bi-person-circle"></i></li>
             <h2 style="color: red;">SHOPING WEBSITE</h2>
 
+
         </ul>
     </nav>
 <header class="header-second">
@@ -70,6 +71,8 @@ if(mysqli_num_rows($data_register)>0){
                     </div>
                 </li>
                 <li><a href="#">All</a></li>
+                <!-- <span><li><input class="form-control" type="search" placeholder="seach product"  style="margin-left:50% ;height:40px;width:200px;border:none;outline:none;text-align:center;color:black"></li></span> -->
+
             </ul>
         </nav>
     </header>
@@ -78,10 +81,10 @@ if(mysqli_num_rows($data_register)>0){
 
 
 <div class="product-item">
-<p><h4 id="product-name-1">Apple </h4></p>
-    <img id="product-img1" src="img/apple.webp">
-    <h3 style="color: blue;">Price - <span id="cost-1">115999</span></h3>
-    <p id="desc-1">128 GB ROM
+<p><h4 id="product-name-1">Apple</h4></p>
+    <img id="product_url-1" src="img/apple.webp">
+    <h3 style="color: blue;">Price - <span id="product-price-1">115999</span></h3>
+    <p id="product-desc-1">128 GB ROM
         15.49 cm (6.1 inch) Super Retina XDR Display
         12MP + 12MP | 12MP Front Camera
         A14 Bionic Chip with Next Generation Neural Engine Processor
@@ -92,36 +95,36 @@ if(mysqli_num_rows($data_register)>0){
     <br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-1" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-1" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 <div class="product-item" >
 <caption><h4 id="product-name-2">Samsung</h4></caption>
-    <img src="img/samsung.webp">
-    <h3 style="color: blue;">Price - <span> 22599</span></h3>
-    <p>6 GB RAM | 128 GB ROM | Expandable Upto 1 TB
+    <img id="product_url-2" src="img/samsung.webp">
+    <h3 style="color: blue;">Price - <span id="product-price-2"> 22599</span></h3>
+    <p id="product-desc-2">6 GB RAM | 128 GB ROM | Expandable Upto 1 TB
          16.26 cm (6.4 inch) HD+ Display 48MP + 8MP + 2MP + 2MP | 13MP Front Camera
          6000 mAh Lithium-ion Batterys MediaTek Helio G80 Processor.1 Year Manufacturer Warranty.
     </p>
     <a href="SecondPage.html">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-2" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-2" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button  class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 
 </div>
 
 
 <div class="product-item" >
 <caption><h4 id="product-name-3">Black Berry</h4></caption>
-    <img src="img/black-berry.webp">
-    <h3 style="color: blue;">Price - 13950</h3>
-    <p>4 GB RAM | 64 GB ROM | Expandable Upto 256 GB
+    <img id="product_url-3" src="img/black-berry.webp">
+    <h3 style="color: blue;">Price -<span id="product-price-3">13950</span></h3>
+    <p id="product-desc-3">4 GB RAM | 64 GB ROM | Expandable Upto 256 GB
         15.21 cm (5.99 inch) Full HD+ Display
         13MP + 13MP | 16MP Front Camera
         4000 mAh Battery
@@ -130,118 +133,122 @@ if(mysqli_num_rows($data_register)>0){
     <a href="ThirdPage.html">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-3" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-3 " class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 <div class="product-item" >
 <caption><h4 id="product-name-4">Campus</h4></caption>
-    <img id="img1" src="img/shose.webp">
-    <h3 style="color: blue;">Price - 650</h3>
-    <p>Nexon-03 White Gym,Sports,Casual,Walking,Stylish Walking Shoes For Men (White, Orange)
+    <img id="product_url-4" src="img/shose.webp">
+    <h3 style="color: blue;">Price -<span id="product-price-4">650</span></h3>
+    <p id="product-desc-4">Nexon-03 White Gym,Sports,Casual,Walking,Stylish Walking Shoes For Men (White, Orange)
     A shoe is an item of footwear intended to protect and comfort.
     </p>
     <a href="#">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-4" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-4" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button  class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 <div class="product-item" >
 <caption><h4 id="product-name-5">T-shirt</h4></caption>
-    <img src="img/tshirt.webp">
-    <h3 style="color: blue;">Price - 950</h3>
-    <p>Men Striped Polo Neck Cotton Blend Blue T-Shirt.A T-shirt (also spelled tee shirt), or tee for short, is a style of fabric shirt named after the T shape of its body and sleeves.
+    <img id="product_url-5" src="img/tshirt.webp">
+    <h3 style="color: blue;">Price -<span id="product-price-5">950</span></h3>
+    <p id="product-desc-5">Men Striped Polo Neck Cotton Blend Blue T-Shirt.A T-shirt (also spelled tee shirt), or tee for short, is a style of fabric shirt named after the T shape of its body and sleeves.
     </p>
     <a href=#">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-5" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-5" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 <div class="product-item" >
 <caption><h4 id="product-name-6">Kurti</h4></caption>
-    <img src="img/womem.webp">
-    <h3 style="color: blue;">Price - 1,050</h3>
-    <p>Kurtis take a top spot in fashionable and adaptable clothing for women with ethnic themes. They are available in a vast range of patterns, designs, themes
+    <img id="product_url-6" src="img/womem.webp">
+    <h3 style="color: blue;">Price -<span id="product-price-6">1050</span></h3>
+    <p id="product-desc-6">Kurtis take a top spot in fashionable and adaptable clothing for women with ethnic themes. They are available in a vast range of patterns, designs, themes
     </p>
     <a href="ThirdPage.html">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-6" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-6" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button  class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 <div class="product-item" >
 <caption><h4 id="product-name-7">LED</h4></caption>
-    <img src="img/tv.webp">
-    <h3 style="color: blue;">Price - 22,998</h3>
-    <p>Featuring Dolby Audio that offers 20 W high-fidelity sound, this TV is sure to enhance your aural experience. So, no matter what kind of content you're watching, you'll be immersed and entertained by this TV's high-quality sound.
+    <img id="product_url-7" src="img/tv.webp">
+    <h3 style="color: blue;">Price -<span id="product-price-7">998</span></h3>
+    <p id="product-desc-7">
+    With Mic:Yes
+    Bluetooth version: 5
+    Wireless range: 10 m
+    Battery life: 35 Hrs | Charging time: 4.5 Hrs
+    Using simple touch controls answer phone calls.
+    With Mic:Yes
+    Bluetooth version: 5
+    Wireless range: 10 m
     </p>
     <a href="#">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-7" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-7" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button  class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 <div class="product-item" >
 <caption><h4 id="product-name-8">Headphone</h4></caption>
-    <img src="img/headphone.webp">
-    <h3 style="color: blue;">Price - 13950</h3>
-    <p>
-
-With Mic:Yes
-Bluetooth version: 5
-Wireless range: 10 m
-Battery life: 35 Hrs | Charging time: 4.5 Hrs
-Using simple touch controls answer phone calls.
-With Mic:Yes
-Bluetooth version: 5
-Wireless range: 10 m
-Battery life: 35 Hrs | Charging time: 4.5 Hrs
-
+    <img id="product_url-8" src="img/headphone.webp">
+    <h3 style="color: blue;">Price -<span id="product-price-8">999</span></h3>
+    <p id="product-desc-8">
+    With Mic:Yes
+    Bluetooth version: 5
+    Wireless range: 10 m
+    Battery life: 35 Hrs | Charging time: 4.5 Hrs
+    Using simple touch controls answer phone calls.
+    With Mic:Yes
+    Bluetooth version: 5
+    Wireless range: 10 m
     </p>
     <a href="#">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-8" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-8" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button  class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 <div class="product-item" >
 <caption><h4 id="product-name-9">Bluetooth</h4></caption>
-    <img src="img/bluetooth.webp">
-    <h3 style="color: blue;">Price - 13950</h3>
-    <p>
+    <img id="product_url-9" src="img/bluetooth.webp">
+    <h3 style="color: blue;">Price -<span id="product-price-9">1299</span></h3>
+    <p id="product-desc-9">
 With Mic:Yes
 11.2mm Bass Boost Driver | 17hrs Total Playback
 Environmental Noise Cancellation | IPX4 Water Resistant
 Multi Device Switch | 88ms Low Latency
 Quick Charge (120mins playback in 10min charge)
-
     </p>
     <a href="#">See more</a><br>
     <form id='myform' method='POST' class='quantity' action='#'>
         <input type='button' value='-' class='qtyminus minus' field='quantity' />
-        <input id="quantity-item-1" type='text' name='quantity' value='1' class='qty' />
+        <input id="product-qty-9" type='text' name='quantity' value='1' class='qty' />
         <input type='button' value='+' class='qtyplus plus' field='quantity' />
     </form>
-    <button id="product-id-9" class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
+    <button class="cart" style="background-color: orange;border:none;color:white;margin-top:10px"><i class="bi bi-cart3"></i>ADD TO CART</button> <button class="buy" style="background-color: red;color:white;border:none"><i class="bi bi-bag"></i>BUY NOW</button>
 </div>
 
 </div>
@@ -250,6 +257,6 @@ Quick Charge (120mins playback in 10min charge)
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+  <!-- <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script> -->
 <script type="text/javascript" src="js/script.js"></script>
 </html>
