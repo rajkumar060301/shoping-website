a7b3a985b79e27f9fdbdc8388fab85af5dd38e50
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-14 15:47:24 +0530

    worked on add product file some chenged this api

--- api/add-product.php
+++ api/add-product.php
@@ -1,7 +1,7 @@
 <?php
 include '../config/config.php';
 
-// if($_POST["action"] == "insert"){
+    $id = $_POST['pid'];
     $name = $_POST['name'];
     $desc = $_POST['desc'];
     $quantity = $_POST['quantity'];
@@ -9,16 +9,16 @@ include '../config/config.php';
     $images = $_POST['images'];
 
 
-    // $query = "SELECT * FROM `cart` WHERE `product_id` = '$id'";
-    // $result = mysqli_query($myConnection,$query);
-    // if (mysqli_num_rows($result) > 0) {
-    //             echo 1;
-    //         }
+    $query = "SELECT * FROM `cart` WHERE `product_id` = '$id'";
+    $result = mysqli_query($myConnection,$query);
+    if (mysqli_num_rows($result) > 0) {
+                echo 1;
+            }
 
-    //         else{
+            else{
 
-                $insert = "INSERT INTO `cart`(`product_name`,`product_description`,`product_quantity`,`product_cost`,`product_img`) 
-                VALUES('$name','$desc',$quantity,'$cost','$images')";
+                $insert = "INSERT INTO `cart`(`product_id`,`product_name`,`product_description`,`product_quantity`,`product_cost`,`product_img`) 
+                VALUES('$id','$name','$desc',$quantity,'$cost','$images')";
                 if(mysqli_query($myConnection,$insert)){
                     echo "Card add in card menu";
                 }
@@ -26,9 +26,9 @@ include '../config/config.php';
                      echo "Data not added in card";
 
 
-                // }
+                }
 
-                // }
+                }
 
-            }
+
 
