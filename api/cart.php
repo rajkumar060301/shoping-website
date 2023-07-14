0ad16c450c67834ed4a5498c451494d7e694c820
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-07-14 15:47:55 +0530

    modified file and use condion of alreay exist product

--- api/cart.php
+++ api/cart.php
@@ -9,7 +9,7 @@ a:hover{text-decoration:none; color:white}
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<!-- <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script> -->
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
     <link rel="styleseet" type="text/css" href="./css/css/bootstrap.min.css"> 
 
@@ -68,8 +68,9 @@ $(document).ready(function () {
 
                     }
                     //  echo $sum;
+                    echo "<tr><td colspan='5' style='color: red;background-color:yellow;'>Total Amount</td><td class='btn btn-success' style='color: red;'> $sum</td></tr>";
+
             echo "</tbody>";
-            echo "<tr><td colspan='5' style='color: red;background-color:yellow;'>Total Amount</td><td class='btn btn-success' style='color: red;'> $sum</td></tr>";
 
         echo "</table>";
 
